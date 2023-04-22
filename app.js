8da3f1b88d71b50494b6c75dbd388dfe55e00b3e
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-04-22 12:33:00 +0200

    New countdown test

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+
+// array for countdown.
+const myArray = [];
+for(let i=9; i>0; i-=2){
+    myArray.push(i);
+}
+
+// HTML element where the countdown will be displayed.
+const countdownElement = document.getElementById("countdown");
+
+// Create a function to display the next countdown value
+function countdown() {
+    //Get the next value from the array.
+    const value = myArray.shift();
+    //Display the value in the HTML element
+    countdownElement.innerHTML = value;
+    // Check if there are any more values in the array.
+    if(myArray.length > 0) {
+        // If there are, set a timeout to display the next value.
+        setTimeout(countdown, 1000);
+    } else {
+        //IF there aren't, display a message that the countdown has fin.
+        countdownElement.innerHTML = "Countdown finished!";
+    }
+}
+// Start the countdown;
+countdown();
+
 alert("Hello, World!");
